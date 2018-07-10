ca41a6c240198acf666997b6f951309f0e233baf
jamalabbasi <jamalabbasi@gmail.com>
2018-07-10 13:15:06 +0500

    Succesfully Add a New Customer To Database From A Form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -39,5 +39,21 @@ namespace Vidly.Controllers
 
             return View(customer);
         }
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customers");
+        }
     }
 }

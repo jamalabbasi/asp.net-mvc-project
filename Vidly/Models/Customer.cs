97e4acf50d1658eafdd1a6914900e35187fddf38
jamalabbasi <jamalabbasi@gmail.com>
2018-07-11 16:10:34 +0500

    Changing Error Messsage for Validation Note only one change

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,7 +10,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        
+        [Required(ErrorMessage ="Please enter customer name.")]
         [StringLength(255)]
         public string Name { get; set; }
 

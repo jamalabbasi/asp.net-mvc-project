0ef6327880c4124fdc42a099960f239e2786ad4f
jamalabbasi <jamalabbasi@gmail.com>
2018-07-18 00:20:42 +0500

    Succesfully Deleting a Customer using Jquery ajax and Web api

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -68,6 +68,19 @@ namespace Vidly.Controllers.Api
 
             _context.SaveChanges();
         }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return NotFound();
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
 

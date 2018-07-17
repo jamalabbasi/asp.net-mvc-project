78214c49b33089f8adb0fb7fe2750c2313fd0f5c
jamalabbasi <jamalabbasi@gmail.com>
2018-07-18 01:21:50 +0500

    Instead of Getting Full view from a server succesfully get only data from a server in json format and build view on client

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -19,7 +19,8 @@ namespace Vidly.Dtos
  
         public byte MembershipTypeId { get; set; }
  
- //        [Min18YearsIfAMember]
+        public MembershipTypeDto MembershipType { get; set; }
+        //        [Min18YearsIfAMember]
         public DateTime Birthdate { get; set; }
     }
 }
